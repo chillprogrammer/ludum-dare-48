c90437baf5a874bd13d01af4491d09f02f4ad3af
Joel Schechter <joels4331@gmail.com>
2021-04-24 10:59:29 -0400

    Sound Manager works

--- src/app/game.ts
+++ src/app/game.ts
@@ -2,16 +2,19 @@ import * as PIXI from "pixi.js"
 import { getServiceByClass } from './services/service-injector.module'
 import { PixiManager } from "./services/pixi-manager/pixi-manager.service";
 import { TextureManager } from './services/texture-manager/texture-manager.service'
+import { SoundManager } from "./services/sound-manager/sound-manager.service";
 
 export class Game {
 
     private app: PIXI.Application;
     private textureManager: TextureManager;
     private pixiManager: PixiManager;
+    private soundManager: SoundManager;
 
     constructor() {
         this.textureManager = getServiceByClass(TextureManager);
         this.pixiManager = getServiceByClass(PixiManager);
+        this.soundManager = getServiceByClass(SoundManager);
 
         this.init();
     }
@@ -22,11 +25,15 @@ export class Game {
      */
     init() {
         this.app = this.pixiManager.getApp();
-        
+
         this.initializeResources();
 
         //Create the game loop.
         this.app.ticker.add(delta => this.gameLoop(delta));
+
+        let sound = this.soundManager.getSound("Ludemdare_More_Bass_v2_Electric_Boogaloo.mp3");
+        sound.loop(true)
+        sound.play();
     }
 
     /**
@@ -42,13 +49,14 @@ export class Game {
      */
     loadTextures() {
         this.textureManager.loadTextureIntoMemory("default.jpg");
+        this.textureManager.removeTextureFromMemory("default.jpg");
     }
 
     /**
     * This function loads any sounds into memory.
     */
     loadSounds() {
-
+        this.soundManager.loadSoundIntoMemory("Ludemdare_More_Bass_v2_Electric_Boogaloo.mp3");
     }
 
     /**
