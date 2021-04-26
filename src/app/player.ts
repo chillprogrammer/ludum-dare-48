f5e8248d77750aae7ff7a805437ea2ebea5c668f
Joel Schechter <joels4331@gmail.com>
2021-04-25 21:38:20 -0400

    updated splash screen and hurt sound effects

--- src/app/player.ts
+++ src/app/player.ts
@@ -2,6 +2,7 @@ import * as PIXI from "pixi.js"
 import { Sprite } from "pixi.js";
 import { PixiManager } from "./services/pixi-manager/pixi-manager.service";
 import { getServiceByClass } from "./services/service-injector.module";
+import { SoundManager } from "./services/sound-manager/sound-manager.service";
 import { TextureManager } from "./services/texture-manager/texture-manager.service";
 
 export class Player {
@@ -22,6 +23,7 @@ export class Player {
     // Services
     private pixiManager: PixiManager;
     private textureManager: TextureManager;
+    private soundManager: SoundManager;
 
     // Useful variables
     private collisionCooldown: number = 0;
@@ -30,6 +32,7 @@ export class Player {
     constructor() {
         this.pixiManager = getServiceByClass(PixiManager);
         this.textureManager = getServiceByClass(TextureManager);
+        this.soundManager = getServiceByClass(SoundManager);
 
         this.init();
     }
@@ -39,10 +42,14 @@ export class Player {
         this.velocity = new PIXI.Point(0, 0);
         this.sprite = new Sprite(this.textureManager.getTexture(this.spritePath));
         this.sprite.position.x = (PixiManager.INITIAL_WIDTH / 2) - this.sprite.width;
-        this.sprite.position.y = (PixiManager.INITIAL_HEIGHT/3) - this.sprite.height;
+        this.sprite.position.y = (PixiManager.INITIAL_HEIGHT / 3) - this.sprite.height;
         this.sprite.interactive = true;
-        
+
         this.app.stage.addChild(this.sprite);
+
+        this.soundManager.loadSoundIntoMemory('air.wav');
+        this.soundManager.loadSoundIntoMemory('air2.wav');
+        this.soundManager.loadSoundIntoMemory('air3.wav');
     }
 
     cleanUp() {
@@ -54,19 +61,19 @@ export class Player {
         this.velocity.y -= this.speed;
     }
 
-    jumpUp(delta: number){ 
-        const player_floor = (PixiManager.INITIAL_HEIGHT/3) - this.sprite.height;
-        if(this.sprite.y >= player_floor){
+    jumpUp(delta: number) {
+        const player_floor = (PixiManager.INITIAL_HEIGHT / 3) - this.sprite.height;
+        if (this.sprite.y >= player_floor) {
             this.sprite.y = player_floor;
             this.verticalSpeed = 0;
             this.jumping = false;
         }
 
-        if(this.jumping){
-            this.verticalSpeed += delta* 1/3;
+        if (this.jumping) {
+            this.verticalSpeed += delta * 1 / 3;
         }
 
-        if(!this.jumping){
+        if (!this.jumping) {
             this.jumping = true;
             this.verticalSpeed = -5;
         }
@@ -75,9 +82,9 @@ export class Player {
     }
 
     moveDown() {
-       this.velocity.y += this.speed;
+        this.velocity.y += this.speed;
     }
-    
+
     moveLeft() {
         this.velocity.x -= this.speed;
     }
@@ -88,9 +95,20 @@ export class Player {
 
     takeDamage(damageAmount: number) {
         if (this.collisionCooldown === this.maxCollisionCooldown) {
-            //console.log(`${damageAmount} damage!`)
             this.collisionCooldown -= 0.01;
             this.health -= damageAmount;
+
+            let soundNum = Math.ceil(Math.random() * 3 + 1);
+            if (soundNum === 1) {
+                let sound = this.soundManager.getSound('air.wav');
+                sound.play();
+            } else if (soundNum === 2) {
+                let sound = this.soundManager.getSound('air2.wav');
+                sound.play();
+            } else if (soundNum === 3) {
+                let sound = this.soundManager.getSound('air3.wav');
+                sound.play();
+            }
         }
     }
 
